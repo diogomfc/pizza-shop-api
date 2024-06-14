c5b07f342a576cd82feaa930cb9805c995bc5fb2
Diogo Silva <diogomfc@hotmail.com>
2024-06-14 18:42:30 -0300

    feat(setup-api): Cria variáveis de ambiente

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from "drizzle-kit";
+import { env } from "./src/env";
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema/*",
   out: "./drizzle",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
 });
