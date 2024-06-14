4cc83a5b47b60cc37303170bb0b79db8feb63068
Diogo Silva <diogomfc@hotmail.com>
2024-06-14 20:16:26 -0300

    feat(db): Cria seed com Drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { env } from "./src/env";
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./src/db/schema/*",
+  schema: "./src/db/schema/index.ts",
   out: "./drizzle",
   dbCredentials: {
     url: env.DATABASE_URL,
