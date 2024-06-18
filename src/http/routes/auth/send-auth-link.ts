741b3a20da5c423e4fd95dfd31d3c69aad139f25
Diogo Silva <diogomfc@hotmail.com>
2024-06-18 19:25:42 -0300

    feat(api-rota): Cria rota POST/authenticate Gera link de autenticação

---
+++ src/http/routes/auth/send-auth-link.ts
@@ -0,0 +1,43 @@
+import Elysia, { t } from 'elysia'
+import { createId } from '@paralleldrive/cuid2'
+import { db } from '@/db/connection-db'
+import { authLinks } from '@/db/schema'
+import { env } from '@/env'
+
+export const sendAuthLink = new Elysia().post(
+  '/authenticate',
+  async ({ body }) => {
+    const { email } = body
+
+    // Verificar se o usuário existe no banco de dados
+    const userFromEmail = await db.query.users.findFirst({
+      where(fields, { eq }) {
+        return eq(fields.email, email)
+      },
+    })
+
+    if (!userFromEmail) {
+      throw new Error('User not found')
+    }
+
+    const authLinkCode = createId()
+
+    await db.insert(authLinks).values({
+      userId: userFromEmail.id,
+      code: authLinkCode,
+    })
+
+    // Enviar um e-mail
+    const authLink = new URL('/auth-links/authenticate', env.API_BASE_URL)
+
+    authLink.searchParams.set('code', authLinkCode)
+    authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
+
+    console.log(authLink.toString())
+  },
+  {
+    body: t.Object({
+      email: t.String({ format: 'email' }),
+    }),
+  },
+)
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,9 @@
 import { Elysia } from 'elysia'
 
 import { createRestaurante } from './routes/restaurants/create-restaurant'
+import { sendAuthLink } from './routes/auth/send-auth-link'
 
-const app = new Elysia().use(createRestaurante)
+const app = new Elysia().use(createRestaurante).use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
