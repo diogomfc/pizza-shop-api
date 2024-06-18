aeb782d73e274f2c6b6e6ca27e09781e4da58c55
Diogo Silva <diogomfc@hotmail.com>
2024-06-18 19:35:58 -0300

    feat(api): Setup da autenticação JWT

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,24 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
+import jwt from '@elysiajs/jwt'
+import cookie from '@elysiajs/cookie'
 
 import { createRestaurante } from './routes/restaurants/create-restaurant'
 import { sendAuthLink } from './routes/auth/send-auth-link'
+import { env } from '@/env'
 
-const app = new Elysia().use(createRestaurante).use(sendAuthLink)
+const app = new Elysia()
+  .use(
+    jwt({
+      secret: env.JWT_SECRET_KEY,
+      schema: t.Object({
+        sub: t.String(),
+        restauranteId: t.Optional(t.String()),
+      }),
+    }),
+  )
+  .use(cookie())
+  .use(createRestaurante)
+  .use(sendAuthLink)
 
 app.listen(3333, () => {
   console.log('🔥 HTTP server running!')
