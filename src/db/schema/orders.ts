fb164e5880668bb435bc5f57725dec447d8d3400
Diogo Silva <diogomfc@hotmail.com>
2024-06-19 18:08:09 -0300

    feat(api-schema): Cria relacionamentos entre pedidos, usuários, restaurantes e itens de pedidos

--- src/db/schema/orders.ts
+++ src/db/schema/orders.ts
@@ -1,6 +1,7 @@
 import { integer, pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
-import { restaurants, users } from '.'
+import { restaurants, users, orderItems } from '.'
+import { relations } from 'drizzle-orm'
 
 export const orderStatusEnum = pgEnum('order_status', [
   'pending',
@@ -10,6 +11,7 @@ export const orderStatusEnum = pgEnum('order_status', [
   'canceled',
 ])
 
+// Crie uma tabela de pedidos
 export const orders = pgTable('orders', {
   id: text('id')
     .$defaultFn(() => createId())
@@ -25,4 +27,21 @@ export const orders = pgTable('orders', {
   status: orderStatusEnum('status').default('pending').notNull(),
   totalInCents: integer('total_in_cents').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
+})
+
+// Crie uma relação entre pedidos, usuários, restaurantes e itens de pedidos
+export const ordersRelations = relations(orders, ({ one, many }) => {
+  return {
+    customer: one(users, {
+      fields: [orders.customerId],
+      references: [users.id],
+      relationName: 'order_customer',
+    }),
+    restaurant: one(restaurants, {
+      fields: [orders.restaurantId],
+      references: [restaurants.id],
+      relationName: 'order_restaurant',
+    }),
+    orderItems: many(orderItems),
+  }
 })
