5bc819a2d5cf8081f2170ee937e1cc218e5cd823
Diogo Silva <diogomfc@hotmail.com>
2024-06-19 17:58:53 -0300

    feat(api-schema): Cria relacionamentos usuários e perdidos

--- src/db/schema/users.ts
+++ src/db/schema/users.ts
@@ -1,8 +1,12 @@
 import { pgEnum, pgTable, text, timestamp } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
+import { relations } from 'drizzle-orm'
+import { restaurants } from './restaurantes'
+import { orders } from './orders'
 
 export const userRoleEnum = pgEnum('user_role', ['manager', 'customer'])
 
+// Crie uma tabela de usuários
 export const users = pgTable('users', {
   id: text('id')
     .$defaultFn(() => createId())
@@ -13,4 +17,16 @@ export const users = pgTable('users', {
   role: userRoleEnum('role').default('customer').notNull(),
   createdAt: timestamp('created_at').notNull().defaultNow(),
   updatedAt: timestamp('updated_at').notNull().defaultNow(),
+})
+
+// Crie uma relação entre usuários e pedidos
+export const usersRelations = relations(users, ({ one, many }) => {
+  return {
+    managedRestaurant: one(restaurants, {
+      fields: [users.id],
+      references: [restaurants.managerId],
+      relationName: 'managed_restaurant',
+    }),
+    orders: many(orders),
+  }
 })
