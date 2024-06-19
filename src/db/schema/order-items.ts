2cab0f6e7cda8fd8e9d0137ebca2d24a757ea872
Diogo Silva <diogomfc@hotmail.com>
2024-06-19 18:11:21 -0300

    feat(api-schema): Cria relacionamentos entre itens de pedidos, pedidos e produtos

--- src/db/schema/order-items.ts
+++ src/db/schema/order-items.ts
@@ -1,7 +1,9 @@
 import { integer, pgTable, text } from 'drizzle-orm/pg-core'
 import { createId } from '@paralleldrive/cuid2'
 import { orders, products } from '.'
+import { relations } from 'drizzle-orm'
 
+// Crie uma tabela de itens de pedidos
 export const orderItems = pgTable('order_items', {
   id: text('id')
     .$defaultFn(() => createId())
@@ -16,4 +18,20 @@ export const orderItems = pgTable('order_items', {
   }),
   priceInCents: integer('price_in_cents').notNull(),
   quantity: integer('quantity').notNull(),
+})
+
+// Crie uma relação entre itens de pedidos, pedidos e produtos
+export const orderItemsRelations = relations(orderItems, ({ one }) => {
+  return {
+    order: one(orders, {
+      fields: [orderItems.orderId],
+      references: [orders.id],
+      relationName: 'order_item_order',
+    }),
+    product: one(products, {
+      fields: [orderItems.productId],
+      references: [products.id],
+      relationName: 'order_item_product',
+    }),
+  }
 })
